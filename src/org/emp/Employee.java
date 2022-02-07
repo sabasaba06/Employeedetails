70f6fd21008400bbb6e102cb16577f9860035332
sabasaba06 <sabari.yellow6@gmail.com>
2022-02-07 18:01:52 +0530

    Folder structure

---
+++ src/org/emp/Employee.java
@@ -0,0 +1,24 @@
+package org.emp;
+
+public class Employee {
+	private void empid() {
+		System.out.println("00IHC");
+	}
+	private void empname() {
+		System.out.println("saba");
+	}
+	private void empdob() {
+		System.out.println("29/11/98");
+	}
+	private void empmail() {
+		System.out.println("csaba0@gmail.com");
+	}
+	public static void main(String[]args) {
+		Employee d =new Employee();
+		d.empdob();
+		d.empid();
+		d.empmail();
+		d.empname();
+	}
+
+}
